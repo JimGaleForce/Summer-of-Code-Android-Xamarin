ea563afb573e82a3a23b22d3b4748a1161f8a5c4
Jim Gale <jimgaleforce@gmail.com>
2018-08-28 16:45:46 -0700

    a02b-added URL, connection, stream, reading from webcall

--- USGSService.cs
+++ USGSService.cs
@@ -1,6 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System.IO;
 using Android.OS;
+using Java.IO;
 using Java.Lang;
+using Java.Net;
+using IOException = Java.IO.IOException;
 
 namespace QuakeX
 {
@@ -8,6 +11,31 @@ namespace QuakeX
     {
         protected override Void RunInBackground(params Void[] @params)
         {
+            StringBuffer data = new StringBuffer();
+
+            try
+            {
+                URL url = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-08-19&endtime=2018-09-09&minmagnitude=6&minlatitude=-30&maxlatitude=-10&minlongitude=-180&maxlongitude=-170");
+                HttpURLConnection connection = (HttpURLConnection)url.OpenConnection();
+                Stream inputStream = connection.InputStream;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                string line = "";
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    data.Append(line);
+                }
+            }
+            catch (MalformedURLException e)
+            {
+                e.PrintStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.PrintStackTrace();
+            }
+
             return null;
         }
     }
