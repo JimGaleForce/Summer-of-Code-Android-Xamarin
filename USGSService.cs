ebfe458814ec66d67b569a2c87fd324361bfe827
Jim Gale <jimgaleforce@gmail.com>
2018-08-28 16:38:40 -0700

    a02a-added new USGSService (thread) task

---
+++ USGSService.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Android.OS;
+using Java.Lang;
+
+namespace QuakeX
+{
+    public class USGSService : AsyncTask<Void,Void,Void>
+    {
+        protected override Void RunInBackground(params Void[] @params)
+        {
+            return null;
+        }
+    }
+}
